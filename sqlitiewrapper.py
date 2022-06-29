f9c0f019787cbfcc1b5f7284448a7c9373e47e54
Furkan Yıldırım <67336740+FurkiYildirim@users.noreply.github.com>
2022-06-29 19:26:19 +0300

    update_row, read optimized

--- sqlitiewrapper.py
+++ sqlitiewrapper.py
@@ -18,25 +18,30 @@ class Log:
 
 
 class DbWrapper:
-    def __init__(self, filename, log="log.txt"):
+    def __init__(self, filename:str, log="log.txt"):
         self.__filename = filename
         self.__logfile = Log(log)
 
-    def ShapeTable(self, paramarr: list, tablename):
+    def ShapeTable(self, paramarr: list, tablename:str) -> bool:
         """
         :param paramarr: Sütunların olduğu kısım
         :param tablename: Table'ın adı
         :return:
         """
         # Bu kısım sütun oluşturmaya yarar
-        DbOverseer = connect(self.__filename)
-        DbCursor = DbOverseer.cursor()
-        DbCursor.execute(f"CREATE TABLE {tablename} ({', '.join(paramarr)})")
-        DbOverseer.commit()
-        DbOverseer.close()
-        self.__logfile.plog(f"{self.__filename}: Created table {tablename} with variables {paramarr}.")
+        try:
+            DbOverseer = connect(self.__filename)
+            DbCursor = DbOverseer.cursor()
+            DbCursor.execute(f"CREATE TABLE {tablename} ({', '.join(paramarr)})")
+            DbOverseer.commit()
+            DbOverseer.close()
+            self.__logfile.plog(f"{self.__filename}: Created table {tablename} with variables {paramarr}.")
+            return True
+        except Exception as err:
+            self.__logfile.plog(f"{self.__filename}: Error: [{err}]")
+            return False
 
-    def AppendLine(self, paramarr, tablename):
+    def AppendLine(self, paramarr:list, tablename:str) -> bool:
         """
 
         :param paramarr: bulunan table'a göre veri eklemeye yarar
@@ -44,30 +49,38 @@ class DbWrapper:
         :return:
         """
         # Bu kısım table'a yeni veriler eklemeye yarar
-        DbOverseer = connect(self.__filename)
-        DbCursor = DbOverseer.cursor()
-        temp = [f"'{i}'" for i in paramarr]
-        DbCursor.execute(f"INSERT INTO {tablename} VALUES ({', '.join(temp)})")
-        DbOverseer.commit()
-        DbOverseer.close()
-        self.__logfile.plog(f"{self.__filename}: Appended line to table '{tablename}' with variables {paramarr}.")
-
-    def GetTable(self, tablename) -> list:
+        try:
+            DbOverseer = connect(self.__filename)
+            DbCursor = DbOverseer.cursor()
+            temp = [f"'{i}'" for i in paramarr]
+            DbCursor.execute(f"INSERT INTO {tablename} VALUES ({', '.join(temp)})")
+            DbOverseer.commit()
+            DbOverseer.close()
+            self.__logfile.plog(f"{self.__filename}: Appended line to table '{tablename}' with variables {paramarr}.")
+            return True
+        except Exception as err:
+            self.__logfile.plog(f"{self.__filename}: Error: [{err}]")
+            return False
+    def GetTable(self, tablename:str) -> list or bool:
         """
 
         :param tablename:
         :return: Liste şeklinde veriler dönecek
         """
         # Bu kısım bulunan şablondaki tüm veriyi çeker
-        DbOverseer = connect(self.__filename)
-        DbCursor = DbOverseer.cursor()
-        DbCursor.execute(f"SELECT * FROM {tablename}")
-        LineInfo = DbCursor.fetchall()
-        DbOverseer.commit()
-        DbOverseer.close()
-        self.__logfile.plog(f"{self.__filename}: Requested table {tablename}.")
-        return LineInfo
-    def update_raw(self, tableName, valueThatChange, point, newValue, findBy):
+        try:
+            DbOverseer = connect(self.__filename)
+            DbCursor = DbOverseer.cursor()
+            DbCursor.execute(f"SELECT * FROM {tablename}")
+            LineInfo = DbCursor.fetchall()
+            DbOverseer.commit()
+            DbOverseer.close()
+            self.__logfile.plog(f"{self.__filename}: Requested table {tablename}.")
+            return LineInfo
+        except Exception as err:
+            self.__logfile.plog(f"{self.__filename}: Error: [{err}]")
+            return False
+    def update_raw(self, tableName:str, valueThatChange:str, point:str, newValue:str, findBy:str) -> bool:
         """
 
         :param tableName:
@@ -78,24 +91,52 @@ class DbWrapper:
         :return:
         """
         # Bu kısım bi değeri değiştirmeye yarıyor
-        DbOverseer = connect(self.__filename)
-        DbCursor = DbOverseer.cursor()
-        DbCursor.execute(f"UPDATE {tableName} SET {valueThatChange} = '{newValue}' WHERE {findBy} = '{point}'")
-        DbOverseer.commit()
-        DbOverseer.close()
-        self.__logfile.plog(f"{self.__filename}: Updated value {newValue}.")
-
-    def DeleteTable(self, tablename):
+        try:
+            DbOverseer = connect(self.__filename)
+            DbCursor = DbOverseer.cursor()
+            DbCursor.execute(f"UPDATE {tableName} SET {valueThatChange} = '{newValue}' WHERE {findBy} = '{point}'")
+            DbOverseer.commit()
+            DbOverseer.close()
+            self.__logfile.plog(f"{self.__filename}: Updated value {newValue}.")
+            return True
+        except Exception as err:
+            self.__logfile.plog(f"{self.__filename}: Error: [{err}]")
+            return False
+    def DeleteTable(self, tablename:str) -> bool:
         """
 
         :param tablename:
         :return:
         """
         # Table'yi silmeye yarar
-        DbOverseer = connect(self.__filename)
-        DbCursor = DbOverseer.cursor()
-        DbCursor.execute(f"DROP TABLE IF EXISTS {tablename}")
-        DbOverseer.commit()
-        DbOverseer.close()
-        self.__logfile.plog(f"{self.__filename}: Deleted table '{tablename}'.")
+        try:
+            DbOverseer = connect(self.__filename)
+            DbCursor = DbOverseer.cursor()
+            DbCursor.execute(f"DROP TABLE IF EXISTS {tablename}")
+            DbOverseer.commit()
+            DbOverseer.close()
+            self.__logfile.plog(f"{self.__filename}: Deleted table '{tablename}'.")
+            return True
+        except Exception as err:
+            self.__logfile.plog(f"{self.__filename}: Error: [{err}]")
+            return False
+
+    def DeleteRow(self, tablename:str, Id:str, value:str) -> bool:
+        """
+
+        :param tablename:
+        :param Id: hangi sütündan silineceği
+        :return:
+        """
+        try:
+            DbOverseer = connect(self.__filename)
+            DbCursor = DbOverseer.cursor()
+            DbCursor.execute(f"DELETE FROM {tablename} WHERE {Id}='{value}'")
+            DbOverseer.commit()
+            DbOverseer.close()
+            self.__logfile.plog(f"{self.__filename}: Deleted value from '{tablename}' value is '{Id}' = '{value}'.")
+            return True
+        except Exception as err:
+            self.__logfile.plog(f"{self.__filename}: Error: [{err}]")
+            return False
 
