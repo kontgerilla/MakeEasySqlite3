08f7fafe2fca5c6b6eac6b0d3eca81d5fc5c98ae
Kontrgerilla <67336740+kontgerilla@users.noreply.github.com>
2023-06-13 21:58:06 +0300

    Update sqlitiewrapper.py
    Update Using and added new functions

--- sqlitiewrapper.py
+++ sqlitiewrapper.py
@@ -1,28 +1,45 @@
-"""
-Created By: ! Kontragerilla ღ ヅ#1019 and Ouburursel#4098
-github ! Kontragerilla ღ ヅ#1019: https://github.com/FurkiYildirim
-github Ouburursel#4098 :  https://github.com/darkmechanism
-"""
-
-from sqlite3 import connect
+# -*- coding: utf-8 -*-
+import mysql.connector
 from datetime import datetime
 
 
-class Log:
-    def __init__(self, filename):
-        self.__filename = open(filename, "a")
+# Bu sınıf MySQL sınıfına child olarak loglamaya yarar. Private bir sınıftır.
+class _Log:
+    def __init__(self, filename: str):
+        self.__filename = open(filename, "a") # Filename dosya konumu olarak tanımlar. Or .../log.txt
+        
 
-    def plog(self, log):
-        date = f'[{datetime.now().year}-{datetime.now().month}-{datetime.now().day}  {datetime.now().hour}.{datetime.now().minute}.{datetime.now().second}]'
-        self.__filename.write(date + " " + log + "\n")
+    def plog(self, log) -> none:
+        '''
+        log: spesifik log yazısı
+        '''
+        date = f'[{datetime.now().year}-{datetime.now().month}-{datetime.now().day}  {datetime.now().hour}.{datetime.now().minute}.{datetime.now().second}]'  # Log yapılan tarihi string olarak tanımlar.
+        self.__filename.write(date + " " + log + "\n")  # Dosyaya yazma kısımı
 
+#  Bu sınıf sql'i daha kullanışlı yapıp loglama yapmaya yarıyor.
+class MySQL(_Log):
+    """
+    DATABASE
+    """
 
-class DbWrapper:
-    def __init__(self, filename:str, log="log.txt"):
-        self.__filename = filename
-        self.__logfile = Log(log)
-
-    def ShapeTable(self, paramarr: list, tablename:str) -> bool:
+    def __init__(self, logfile, host, user, pw, db):
+        # Log sınıfının super fonksiyonu
+        super().__init__(filename=logfile)
+        try:
+            # Bağlanılacak database giriş bilgileri.
+            self._DB = mysql.connector.connect(
+                host=host,
+                user=user,
+                password=pw,
+                database=db
+            )
+            self.__logfile.plog(f"{self.__filename}: Connected to {_DB}")
+        except Exception:
+            raise Exception("Did not connect creation with database")
+        self.__logfile = Log(logfile)
+        self.__filename = db
+    # Database içinde istenilen sütunları içeren bir tablo oluşturur. İstenilen sütunlar liste türünde girilmelidir.
+    def shape_table(self, paramarr: list, tablename: str) -> bool:
         """
         :param paramarr: Sütunların olduğu kısım
         :param tablename: Table'ın adı
@@ -30,59 +47,75 @@ class DbWrapper:
         """
         # Bu kısım sütun oluşturmaya yarar
         try:
-            DbOverseer = connect(self.__filename)
-            DbCursor = DbOverseer.cursor()
-            DbCursor.execute(f"CREATE TABLE {tablename} ({', '.join(paramarr)})")
-            DbOverseer.commit()
-            DbOverseer.close()
+            query = f"CREATE TABLE {tablename} ({', '.join(paramarr)})"
+            cursor = self._DB.cursor()
+            cursor.execute(query)
+
+            self._DB.commit()
             self.__logfile.plog(f"{self.__filename}: Created table {tablename} with variables {paramarr}.")
             return True
         except Exception as err:
             self.__logfile.plog(f"{self.__filename}: Error: [{err}]")
-            return False
-
-    def AppendLine(self, paramarr:list, tablename:str) -> bool:
+             return False
+            
+    # Burada database içinde istenilen tabloya satır ekleniyor.
+    # ISTENILEN SATIRI EKLEMEK ICIN TABLDOA BULUNAN SUTUN KADAR DEGER EKLNMEK ZORUNDA!
+    # Geri dönen değer başarılı ve başarsız olarak bool değeri dönüyor.
+    def append_line(self, paramarr: list, tablename: str) -> bool:
         """
-
+        :rtype: object
         :param paramarr: bulunan table'a göre veri eklemeye yarar
         :param tablename: table isim
         :return:
         """
         # Bu kısım table'a yeni veriler eklemeye yarar
         try:
-            DbOverseer = connect(self.__filename)
-            DbCursor = DbOverseer.cursor()
+            query =f"INSERT INTO {tablename} VALUES ({', '.join(temp)})"
+            cursor = self._DB.cursor()
             temp = [f"'{i}'" for i in paramarr]
-            DbCursor.execute(f"INSERT INTO {tablename} VALUES ({', '.join(temp)})")
-            DbOverseer.commit()
-            DbOverseer.close()
+            cursor.execute(query)
+            self._DB.commit()
             self.__logfile.plog(f"{self.__filename}: Appended line to table '{tablename}' with variables {paramarr}.")
             return True
         except Exception as err:
             self.__logfile.plog(f"{self.__filename}: Error: [{err}]")
             return False
-    def GetTable(self, tablename:str) -> list or bool:
+    
+    # DATABASE  içinde bulunan istenile tablodaki tüm satır değerlerini alıyor.
+    # her satır bir tane sözlüktür, her elemean satırdır. Her sözlükteki anahtar kelime sutünu ifade ediyor. Değeri ise sutundaki satır değerini ifade ediyor
+    def get_table(self, tablename: str) -> list or bool:
         """
-
         :param tablename:
         :return: Liste şeklinde veriler dönecek
         """
+        dict_result = []  # => [{}]
         # Bu kısım bulunan şablondaki tüm veriyi çeker
         try:
-            DbOverseer = connect(self.__filename)
-            DbCursor = DbOverseer.cursor()
-            DbCursor.execute(f"SELECT * FROM {tablename}")
-            LineInfo = DbCursor.fetchall()
-            DbOverseer.commit()
-            DbOverseer.close()
+            query = f"SELECT * FROM {tablename}"
+            cursor = self._DB.cursor()
+            cursor.execute(query)
+            LineInfo = cursor.fetchall()
+            desc = cursor.description
+            colnames = []
+            for _ in desc:
+                colnames.append(_[0])
+
+            self._DB.commit()
             self.__logfile.plog(f"{self.__filename}: Requested table {tablename}.")
-            return LineInfo
+
+            for _ in LineInfo:
+                dict_result.append(dict(zip(colnames, _)))
+
+            return dict_result  # [{}]
+
         except Exception as err:
             self.__logfile.plog(f"{self.__filename}: Error: [{err}]")
             return False
-    def update_raw(self, tableName:str, valueThatChange:str, point:str, newValue:str, findBy:str) -> bool:
+    
+    # Burda istenilen satırdaki istenilen değeri değiştirmeye yarayan işlemler yapılıyor.
+    # Fonkisyon açıklamasındaki değerleri okuyunuz.
+    def update_raw(self, tableName: str, valueThatChange: str, point: str, newValue: str, findBy: str) -> bool:
         """
-
         :param tableName:
         :param valueThatChange: Sütun ismi
         :param point: Neye göre bulunancağının beklenen değeri
@@ -92,51 +125,137 @@ class DbWrapper:
         """
         # Bu kısım bi değeri değiştirmeye yarıyor
         try:
-            DbOverseer = connect(self.__filename)
-            DbCursor = DbOverseer.cursor()
-            DbCursor.execute(f"UPDATE {tableName} SET {valueThatChange} = '{newValue}' WHERE {findBy} = '{point}'")
-            DbOverseer.commit()
-            DbOverseer.close()
+            query = f"UPDATE {tableName} SET {valueThatChange} = '{newValue}' WHERE {findBy} = '{point}'"
+            cursor = self._DB.cursor()
+            cursor.execute(query)
+            self._DB.commit()
             self.__logfile.plog(f"{self.__filename}: Updated value {newValue}.")
             return True
         except Exception as err:
             self.__logfile.plog(f"{self.__filename}: Error: [{err}]")
             return False
-    def DeleteTable(self, tablename:str) -> bool:
+    
+    # DATABASE'deki isteline tabloyu siler.
+    def delete_table(self, tablename: str) -> bool:
         """
-
         :param tablename:
         :return:
         """
         # Table'yi silmeye yarar
         try:
-            DbOverseer = connect(self.__filename)
-            DbCursor = DbOverseer.cursor()
-            DbCursor.execute(f"DROP TABLE IF EXISTS {tablename}")
-            DbOverseer.commit()
-            DbOverseer.close()
+            query = f"DROP TABLE IF EXISTS {tablename}"
+            cursor = self._DB.cursor(query)
+            cursor.execute()
+            self._DB.commit()
             self.__logfile.plog(f"{self.__filename}: Deleted table '{tablename}'.")
             return True
         except Exception as err:
             self.__logfile.plog(f"{self.__filename}: Error: [{err}]")
             return False
-
-    def DeleteRow(self, tablename:str, Id:str, value:str) -> bool:
+    
+    # Istenilen tablodaki istenilen satırı silmeye yarar.
+    def delete_row(self, tablename: str, colname: str, value: str) -> bool:
         """
-
         :param tablename:
-        :param Id: hangi sütündan silineceği
+        :param colname: hangi sütündan silineceği
         :return:
         """
         try:
-            DbOverseer = connect(self.__filename)
-            DbCursor = DbOverseer.cursor()
-            DbCursor.execute(f"DELETE FROM {tablename} WHERE {Id}='{value}'")
-            DbOverseer.commit()
-            DbOverseer.close()
-            self.__logfile.plog(f"{self.__filename}: Deleted value from '{tablename}' value is '{Id}' = '{value}'.")
+            query = f"DELETE FROM {tablename} WHERE {colname}='{value}'"
+            cursor = self._DB.cursor()
+            cursor.execute(query)
+            self._DB.commit()
+            self.__logfile.plog(f"{self.__filename}: Deleted value from '{tablename}' value is '{colname}' = '{value}'.")
             return True
         except Exception as err:
             self.__logfile.plog(f"{self.__filename}: Error: [{err}]")
             return False
+    
+    # Tablo içindeki istenilen satır bilgisini alır ve geri döner. Liste türü.
+    def get_specific_row(self, tablename, colname, colname2, value):
+        """
+        Colname istenilen sütündaki verdiğimiz id ve valueler kullanılarak satır döndürülecek sözlük şeklinde
+        :param tablename:
+        :param Id:
+        :param value:
+        :return:
+        """
 
+        # TODO: spesifik tablo alınacak
+        try:
+            # db.get_specific_row(tablename="users", colname="passwords", colname2="username", value="60562eb61e2bac4f17460fa0dad443d7e8db6e61ba98dbbb954872c4d7b34bb5")
+            query = f"SELECT {colname} FROM {tablename} WHERE {colname2}='{value}'"
+            cursor = self._DB.cursor()
+            cursor.execute(query)
+            data = cursor.fetchone()
+            self.__logfile.plog(f"{self.__filename}: Get value from '{tablename}' value is '{colname2}' = '{value}'.")
+            return data
+        except Exception as err:
+            self.__logfile.plog(f"{self.__filename}: Error: [{err}]")
+            return False
+            
+    # TODO: Daha fazla fonksiyon eklenebilir.
+
+
+if __name__ == '__main__':
+    # Alacağınız hataları log dosyasına bakarak öğrenebilirsiniz.
+    
+    # Örnek sınıf tanımlama
+    # xampp benzeri uygulamalar kullanarak serveri aktif hale getiriniz. Yoksa bağlantı gerçekleşemez.
+    # var olan bir olan bir log dosyası olmak zorunda değildir. Oluşturulacaktır.
+    print('Databaseye bağlanılıyor')
+    db = MySQL(logfile="./log/log.txt", user="root", host="localhost", pw="", db="blog")
+    print("Databaseye bağlandı")
+    # Örnek Tablo oluşturma, paramarr sutunları ifade ediyor
+    db.shape_table(paramarr=['id','isim','soyisim'], tablename='kullanicilar')
+    
+    # Tabloya veri ekleme 'kullanicilar' tablosuna. paramarr elemanları sutun sayısı kadar olmak zorunda
+    db.append_line(tablename='kullanicilar', paramarr=['id1', 'furkan', 'yıldırım'])
+    print("veri eklendi")
+    db.append_line(tablename='kullanicilar', paramarr=['id2', 'Metehan', 'Tüfek'])
+    print("bir veri daha eklendi")
+    # Tablodaki verileri alma fonksiyonu 'kulanicilar tablosundan'
+    # Geri dönüş değeri liste şeklindedir.
+    print("Veriler alınıyor")
+    data = db.get_table(tablename='kullanicilar')
+    print(data)
+    print("Veriler alındı")
+    # Tablodaki veriyi güncelleme, örnein soyisimi değiştirelim.
+    # ValueThatChange : değişecek sutunun isimidir.
+    # NewValue : Sutunda bulunan değerimizin artık yeni değeridir. Mesela Furkan ise önceki değeri Artık Mete diye değişecek gibi.
+    # findBy : O satırı hangi sutundaki değere bakılarak bulacağını belirtiyor.
+    # point : findBy sutunu içindeki isetnilen değeri ifade ediyor.
+    print("Kullancı bilgisi değiştiriliyor")
+    db.update_raw(tablename="kullanicilar", ValueThatChange="soyisim", NewValue="Demir", findBy="id", point="id1")
+    print(db.get_table(tablename='kullanicilar'))
+    print("Kullanıcı bilgisi değiştiriliyor")
+    # Tablo içerisinde istenilen SUTUNUN içerisindeki değeri almaya yarar.
+    # Tüm sutunları  almak yerine sadece bir tane sutunun içideki bir tane değeri alırız.
+    # colname : istenilen sutunun içerisindeki istediğimiz değer.
+    # colname2 : colname içidenki değeri bulmamaız için kesin olarak bildiğimiz bir sutunu kullanırız
+    # value: colname2 içidneki eşsiz olan bir değerdir.
+    # Burada 'id2' değerine sahip oan kullanıcı kişisinin ismini alırıyoruz.
+    print("Özel bir sutun deği alındı")
+    print(db.get_specific_row(tablename='kullanicilar', colname='isim', colname2='id', value='id2'))
+    
+    # Tablo içeisinde istenile değeri silmek için kulkanılır.
+    # colname : bileln sutun referansı
+    # value : sutunun bildiğimiz değeri
+    # örneğin idsini bildiğimiz furkan kullanıcısını silelim
+    print("Bir kullanıcı silindi")
+    db.delete_row(tablename='kullanicilar', colname='id', value='id1')
+    print(db.get_table(tablename='kullanicilar'))
+    
+    # İstediğimiz var olan tabloyu silemye yarar.
+    print('"kullanicilar" tablosu sildindi')
+    db.delete_table(tablename='kullanicilar')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
